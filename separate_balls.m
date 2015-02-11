f8a4bb2cf44206f1864017a30bcc7ffe35e7eae7
Dirzys <l.dirzys@yahoo.com>
2015-02-11 19:07:02 +0000

    Making balls recognition work faster

--- separate_balls.m
+++ separate_balls.m
@@ -1,37 +1,39 @@
 function [ substracted_frame ] = separate_balls( substracted_frame, masked_image )
     
     % Find all the objects and their properties present in the frame
-    props = regionprops(substracted_frame, 'pixelList', 'BoundingBox', 'Eccentricity','Centroid');
+    props = regionprops(substracted_frame, 'pixelList', 'BoundingBox', 'Eccentricity', 'Centroid');
     % Separate balls from each connected component that seems to have
     % more than one ball in it
-        
+    allPixels = int16.empty;
     for i = 1 : size(props, 1)
         % First check if there might be more than one ball in the component
-        pixelLists = props(i).PixelList;
         if props(i).Eccentricity < 0.3
            continue
         end
-        % Create a image with only one visible component in it
-        current_component = substracted_frame;
-        current_component(substracted_frame ~= -1) = 0;
-        for j = 1 : size(pixelLists, 1)
-            current_component(pixelLists(j, 2), pixelLists(j, 1)) = 100;
-        end
-        % Create a mask of this component
-        mask = repmat(current_component, [1, 1, 3]);
-        current_masked_image = masked_image;
-        current_masked_image(~mask) = 0;
-        %hsv_i = rgb2hsv(current_masked_image);
-        %imshow(hsv_i)
-        %[X_no_dither,map]= rgb2ind(current_masked_image,4,'nodither');
-        %imshow(X_no_dither, map)
+        allPixels = [allPixels; props(i).PixelList];
+    end
+    
+    % Create an image with visible components that might have more than one
+    % ball in it
+    current_component = substracted_frame;
+    current_component(substracted_frame ~= -1) = 0;
+    for i = 1 : size(allPixels, 1)
+        current_component(allPixels(i, 2), allPixels(i, 1)) = 255;
+    end
+    % Create a mask of these components
+    mask = repmat(current_component, [1, 1, 3]);
+    current_masked_image = masked_image;
+    current_masked_image(~mask) = 0;
+    %hsv_i = rgb2hsv(current_masked_image);
+    %imshow(hsv_i)
+    %[X_no_dither,map]= rgb2ind(current_masked_image,4,'nodither');
+    %imshow(X_no_dither, map)
         
-        % Separate balls in the component
-        separated_component = separate_connected_component(current_masked_image);
-        % Apply changes on the original substracted frame
-        for j = 1 : size(pixelLists, 1)
-            substracted_frame(pixelLists(j, 2), pixelLists(j, 1)) = separated_component(pixelLists(j, 2), pixelLists(j, 1));
-        end
+    % Separate balls in the components
+    separated_component = separate_connected_component(current_masked_image);
+    % Apply changes on the original substracted frame
+    for i = 1 : size(allPixels, 1)
+        substracted_frame(allPixels(i, 2), allPixels(i, 1)) = separated_component(allPixels(i, 2), allPixels(i, 1));
     end
     
 %     mask = repmat(substracted_frame, [1, 1, 3]);
