8bb13c67ca35386513cffaf54ba212eb57a4c9d6
H. Chase Stevens <chase@chasestevens.com>
2015-02-11 01:02:38 +0000

    Add functions for drawing lines between tracked objects

---
+++ draw_line.m
@@ -0,0 +1,8 @@
+function [] = draw_line( x1, y1, x2, y2 )
+	
+    x_points = linspace(x1, x2, 50);  
+    y_points = linspace(y1, y2, 50); 
+
+    plot(x_points, y_points, 'Color', 'red');
+    
+end
---
+++ plot_paths.m
@@ -0,0 +1,22 @@
+function [] = plot_paths( tracked_balls, time )
+
+    INITIAL_TIME = 25;
+
+    for t = time : -1 : INITIAL_TIME + 1
+        num_objects = max(size(tracked_balls{t}));
+
+        for obj_i = 1 : num_objects
+            obj = tracked_balls{t}{obj_i};
+            
+            cur_object_count = max(size(tracked_balls{time}));
+            for obj_j = 1 : cur_object_count
+                cur_obj = tracked_balls{time}{obj_j};
+
+                if strcmp(obj.id, cur_obj.id)
+                    draw_line(obj.x, obj.y, obj.prev_x, obj.prev_y); 
+                    break
+                end
+            end
+        end
+    end
+end
